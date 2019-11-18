afe30bc7814f5a01f90ecdcbf1040410eea018a4
Piotr Trochymiak <Piotr@Trochymiak.net>
2019-11-18 21:45:46 +0100

    correction of css styles and description of the data source

--- src/App.svelte
+++ src/App.svelte
@@ -9,13 +9,6 @@
     max-width: 240px;
     margin: 0 auto;
   }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
   .pep {
     text-align: right;
     padding-top: 3rem;
@@ -35,6 +28,5 @@
     <br />
     <a href="https://trochymiak.net">^p^</a>
   </div>
-  power by
-  <a href="https://svelte.dev" target="_blank">Svelte</a>
+
 </main>
